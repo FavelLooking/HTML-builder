0db2f63c724b50a5b0c62090fb807d6810170336
FavelLooking <intwestpol@gmail.com>
2024-01-18 23:33:35 +0400

    feat: solve read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const readableStream = fs.createReadStream(
+  path.join(__dirname, '/text.txt'),
+  'utf-8',
+);
+readableStream.on('data', (chunk) => {
+  process.stdout.write(chunk);
+});
